38136abebdc3265549feccc3d9a3742eacb2523a
Julian Montero <71648137+JulianMGonzalez@users.noreply.github.com>
2021-07-06 18:48:12 -0500

    css added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import './App.css'
 
 function App() {
   return (
@@ -6,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola!
         </p>
         <a
           className="App-link"
